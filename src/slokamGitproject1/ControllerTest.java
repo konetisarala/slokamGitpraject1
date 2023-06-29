abb420ab3717777bf9c4771a5fb7c29a47fe1247
konetisarala <konetisarala92@gmail.com>
2023-06-29 23:35:04 +0530

    my first commit

---
+++ src/slokamGitproject1/ControllerTest.java
@@ -0,0 +1,23 @@
+package slokamGitproject1;
+
+public class ControllerTest {
+	
+	public void Test1() {
+		
+        System.out.println("Test1()");
+	
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		ControllerTest ControllerTest = new ControllerTest();
+		ControllerTest.Test1();
+		
+		
+	}
+	
+	
+}
+	
